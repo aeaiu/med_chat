4e325d6b28af7aa8c576764658c38b3caa9eee66
Amanda Alonzo <amanda.e.alonzo@gmail.com>
2024-11-24 09:49:47 -0800

    first commit

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ trainer = ChatterBotCorpusTrainer(bot)
 # put the yaml file under: \.venv\Lib\site-packages\chatterbot_corpus\data\custom
 # todo: change from short for prod
 #  only do this once for web app because takes some times
-trainer.train("chatterbot.corpus.custom.medical_short")
+#  trainer.train("chatterbot.corpus.custom.medical_short")
 
 @app.route('/')
 def homepage():
